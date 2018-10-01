1dcd48f4bf7d40dc34ac707bd35648bb454ed0d4
carlos <carlos@sobremesa>
2018-10-01 12:12:25 +0200

    Compilar en open cobol

--- src/main/base-bin/SeguridadVER.sh
+++ src/main/base-bin/SeguridadVER.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-java -cp ${INSTALACION}/seguridad-basica-1.1/seguridad-basica-dkt-1.1-Z.jar seguridadver.Main ${*}
+java -cp ${INSTALACION}/seguridad-basica-1.1/seguridad-basica-dkt-1.1-Z.jar mirarversion.Main ${*}
 
